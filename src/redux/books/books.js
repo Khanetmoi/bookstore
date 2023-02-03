cc38de3f5cd2e726a2183d40358994287ade5883
Inayat-khan <khanetmoi@gmail.com>
2023-02-03 09:20:01 +0000

    state changed

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,23 +1,65 @@
 const ADD = 'bookstore/books/addBook';
 const REMOVE = 'bookstore/books/removeBook';
 
-const initialState = [];
+const initialState = {
+  books: [
+    {
+      id: 1,
+      category: 'action',
+      title: 'Titanic',
+      author: 'Winfred',
+    },
+    {
+      id: 2,
+      category: 'action',
+      title: 'Titanic',
+      author: 'Winnie',
+    },
+    {
+      id: 3,
+      category: 'action',
+      title: 'tanic',
+      author: 'Winfred',
+    },
+    {
+      id: 4,
+      category: 'action',
+      title: 'Titan',
+      author: 'Winfred',
+    },
+  ]
+};
 
 export default function booksReducer(state = initialState, action) {
   switch (action.type) {
-    case ADD:
-      return [...state, action.payload];
+    case ADD: {
+      const newbook = [...state.books, action.payload];
+      return {...state.books, books: newbook};
+    }
+      
     case REMOVE:
-      return state.items.filter((item) => item.id !== action.id);
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload.id),
+      }
+      
     default:
       return state;
   }
 }
 
-export const addBook = () => ({
+export const addBook = (title, author) => ({
   type: ADD,
+  payload: {
+    title,
+    author,
+    id
+  }
 });
 
-export const removeBook = () => ({
+export const removeBook = (id) => ({
   type: REMOVE,
+  payload: {
+    id
+  }
 });
