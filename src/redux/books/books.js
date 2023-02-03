fd4556740c1ae81728f08cbe4938c7b33e44f3f7
Inayat-khan <khanetmoi@gmail.com>
2023-02-03 15:43:21 +0000

    refactor the reducers

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,49 +2,33 @@ import { v4 as uuidv4 } from 'uuid';
 
 const ADD = 'bookstore/books/addBook';
 const REMOVE = 'bookstore/books/removeBook';
+const FECTH = 'bookstore/books/fechBooks';
 
-const initialState = {
-  books: [
-    {
-      id: uuidv4(),
-      category: 'action',
-      title: 'Titanic',
-      author: 'Winfred',
-    },
-    {
-      id: uuidv4(),
-      category: 'action',
-      title: 'Titanic',
-      author: 'Winnie',
-    },
-    {
-      id: uuidv4(),
-      category: 'action',
-      title: 'tanic',
-      author: 'Winfred',
-    },
-    {
-      id: uuidv4(),
-      category: 'action',
-      title: 'Titan',
-      author: 'Winfred',
-    },
-  ],
-};
+const Api = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/qRq5MNlV4Ck1eZaDEPnz';
+
+const initialState = [];
+
 
 export default function booksReducer(state = initialState, action) {
   switch (action.type) {
-    case ADD: {
-      const newbook = [...state.books, action.payload];
-      return { ...state.books, books: newbook };
-    }
+    case `${ADD}/fulfilled`: 
+      return action.payload;
 
-    case REMOVE:
-      return {
-        ...state,
-        books: state.books.filter((book) => book.id !== action.payload.id),
-      };
+    case `${REMOVE}/fulfilled`:
+      return
+         state.filter((book) => book.id !== action.payload);
+    case `${FETCH}/fulfilled`: 
+      return Object.keys(action.payload).map((key)=>{
+        const {title, author, category} = action.payload[key][0];
+        return {
+          item_id: key,
+          title,
+          author,
+          category,
+        }
+      })
 
+      
     default:
       return state;
   }
