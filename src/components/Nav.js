049490808bf0f9e5e3327aefb44cad2cd0c10478
Inayat-khan <khanetmoi@gmail.com>
2023-04-07 22:30:52 +0000

    remove unnecessary code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './books/books.css';
 
 const Nav = () => (
-  <div className="header">
-    <h1 className="logo" to="/">
-      Bookstore CMS
-    </h1>
+  <div>
+    <h1>Bookstore CMS</h1>
     <nav>
-      <Link className="navItem" to="/">Books</Link>
-      <Link className="navItem" to="/Category">Category</Link>
+      <Link to="/">Books</Link>
+      <Link to="/Category">Category</Link>
     </nav>
 
   </div>
