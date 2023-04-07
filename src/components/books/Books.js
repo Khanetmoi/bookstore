5199e3a14b97e43ce303cc9d72580bff22fa0f90
Inayat-khan <khanetmoi@gmail.com>
2023-04-07 22:33:34 +0000

    remove code

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,26 +1,41 @@
-import { useSelector, useDispatch } from 'react-redux';
-import React, { useEffect } from 'react';
-import InputValue from './InputValue';
+import React, { useState } from 'react';
 import UnitBook from './UnitBook';
-import { fetchBooks } from '../../redux/books/books';
+import InputValue from './InputValue';
 
 const Books = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchBooks());
-  }, [dispatch]);
-
-  const addedBooks = useSelector((state) => state.books);
+  const [books] = useState([
+    {
+      id: 1,
+      category: 'action',
+      title: 'Titanic',
+      author: 'Winfred',
+    },
+    {
+      id: 2,
+      category: 'action',
+      title: 'Titanic',
+      author: 'Winnie',
+    },
+    {
+      id: 3,
+      category: 'action',
+      title: 'tanic',
+      author: 'Winfred',
+    },
+    {
+      id: 4,
+      category: 'action',
+      title: 'Titan',
+      author: 'Winfred',
+    },
+  ]);
   return (
-    <div className="booklistContainer">
-      {addedBooks.map((book) => (
-        <UnitBook
-          key={book.item_id}
-          title={book.title}
-          author={book.author}
-          id={book.item_id}
-        />
-      ))}
+    <div>
+      <ul>
+        {books.map((book) => (
+          <UnitBook key={book.id} id={book.id} book={book} />
+        ))}
+      </ul>
       <InputValue />
     </div>
   );
